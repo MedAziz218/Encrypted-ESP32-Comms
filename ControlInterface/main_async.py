39262821ba973affc64b0a94efb630c938d8f674
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2025-01-22 00:37:31 +0100

    finsihed esp32 code next step is to implement the python code

--- ControlInterface/main_async.py
+++ ControlInterface/main_async.py
@@ -1,6 +1,37 @@
 import asyncio
 from asyncserial import Serial
+SEPERATOR = "\n"
 
+private_key_pem = """
+-----BEGIN RSA PRIVATE KEY-----
+MIICWwIBAAKBgQCIa5Mzcik4KH4oG9p5uoRjpZbVQujAP4Ey6GkLAhvbur5AHi3O
+ZwFVPhXDkQBvvFyM/nU+b/muf1w14IGlgvvh5NFXaZiXu/fNZIdCUG2Pz+pQURZo
+BIAuT+SL797ZYKFMRf1RaWR8iImOP1sFftEJDan3h8ZN/f8TRhwjnIjxRQIDAQAB
+AoGAe6sht2qPaWRz8UJGzUFDkN3lHZFZVCZ9pjvANgWEYa4pmBCDr+/66l6s9iv1
+/FUChaKLgL2b2A+G9SSAAx353hwP3Rqs5os/RBGtmrJux7cf0H9/sYoKW8eJQ9BP
+ul0FIYQErGgKgBMD4vdmsJEx7vIftgs28x9jfPxokhbzJOECQQDD5bUcqcmtDN9a
+lQAHVmevRF8nZevxg/sIM4iuvnSvrV9lGVWgf4xGXJv81auarAhlwno8A6kdbVC2
+0iJ9KqxtAkEAskZhTlm30a3yryUXFoSz9U1SN3beizbhboLACU5MUznzyzGwidrB
+hFTQHmD3Y4LY0J8EJlDAl05CKEN7lvihOQJAWbNAOCzCzTuctoSNq85z0bxz+b1g
+yYlOlFXMm39YPO0dRlTQcZqV584WGzLXzg5CFh50DDD86h2ZHO2hn0DADQJAQC2Y
+ECW6SBDQAf9fPWsOgeuRAoiXexSJuUf2rCL01S1St76uqCIJcoM53QXZaYiMVyY3
+zzdY7d9tb6NDlcjx+QJAVeSi5Gsy7r8swU3/ZgIqRODLxqlrJ7/70KCmpIR2XEFZ
+nLUjgWIoq/Flstca6evDPFDgMCBIHNQoEHFpC0wHUw==
+-----END RSA PRIVATE KEY-----
+"""
+public_key_pem = """
+-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgFs44W15Opn0ruNjtl8fY6eNmQir
+3AsKqj2UmZ7ShLegwob3oN1lsn0keL401eOzjv2t7ObD/stszZao4Ugmccb83ITj
+EmfvHgH5rixn4+WT8sa3KW265IrnlfHjVRpGxRqGISf9ClyiK01acc+2EbUHqXyP
++JYQwWf4+ooCYDgFAgMBAAE=
+-----END PUBLIC KEY-----
+"""
+def remove_last_n_chars(input_str: str, n: int) -> str:
+    """Remove the last n characters from the input string."""
+    if n <= 0:
+        return input_str
+    return input_str[:-n] if n < len(input_str) else ""
 
 async def listener_routine(serial_connection: Serial, raw_input_queue: asyncio.Queue):
     """Task to read from the serial connection."""
@@ -10,15 +41,14 @@ async def listener_routine(serial_connection: Serial, raw_input_queue: asyncio.Q
             received_bytes = await serial_connection.read()  # Read a line
             received_str = received_bytes.decode("utf-8", errors="ignore")
             raw_input_queue.put_nowait(received_str)
-            # print("[+] Serial read: {}".format(line_str))
+            # print("[+] Serial read: {}".format(received_str))
             await asyncio.sleep(0.001)  # Adjust sleep if needed
     except asyncio.CancelledError:
         print("[!] Serial reader task cancelled. Exiting...")
         raise  # Re-raise to allow proper cleanup
 
-
 async def read_until_seperator_routine(
-    serial_connection: Serial, raw_input_queue: asyncio.Queue, sep: str = "abc"
+    serial_connection: Serial, raw_input_queue: asyncio.Queue, sep: str = SEPERATOR
 ):
     """Process input from the serial reader."""
     try:
@@ -33,9 +63,11 @@ async def read_until_seperator_routine(
                     inp += c
                     if inp.endswith(sep):
                         #TODO: send the input to the received messages queue
+                        inp = remove_last_n_chars(inp, len(sep))
                         print(inp, end="", flush=True)
+                        #todo: remove this later , only for testing
                         await serial_connection.write(
-                            f"received <{inp}>\n".encode("utf-8")
+                            f"{inp}{sep}".encode("utf-8")
                         )
                         inp = ""
 
@@ -60,7 +92,7 @@ async def main(COM_port="COM4"):
             listener_routine(serial_connection, input_queue)
         )
         handler_task = asyncio.create_task(
-            read_until_seperator_routine(serial_connection, input_queue)
+            read_until_seperator_routine(serial_connection, input_queue,SEPERATOR)
         )
         # Run until interrupted
         await asyncio.sleep(float("inf"))
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,310 @@
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
+// This example code is in the Public Domain (or CC0 licensed, at your option.)
+// By Evandro Copercini - 2018
 //
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+// This example creates a bridge between Serial and Classical Bluetooth (SPP)
+// and also demonstrate that SerialBT have the same functionalities of a normal Serial
 
 #include "BluetoothSerial.h"
+#include <mbedtls/pk.h>
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/base64.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
+// ------------------------------------------------------------------
+const String SEPERATOR = "\n";
 
+const char *private_key_pem = R"(
+-----BEGIN RSA PRIVATE KEY-----
+MIICWgIBAAKBgFs44W15Opn0ruNjtl8fY6eNmQir3AsKqj2UmZ7ShLegwob3oN1l
+sn0keL401eOzjv2t7ObD/stszZao4Ugmccb83ITjEmfvHgH5rixn4+WT8sa3KW26
+5IrnlfHjVRpGxRqGISf9ClyiK01acc+2EbUHqXyP+JYQwWf4+ooCYDgFAgMBAAEC
+gYAiQxfwTTMkdhFl2KK70YdVfEp5RktsXkIYxQJ586njal8F4GYsIbFLbXJoRmH7
+lwpi33t2JTFC6IfDSYTr23yqBJ/6DKiCa4ZsSQPd06iqz2aDHc4Bq7D2ZQMdkOMK
+vpCHpTUgKW1sHYLzqzGMURMHkqBvGA39qZ3TbdBZmZuk9QJBAJ2WoSiis3FA8YUm
+digwYxNepdyxXQu5f6DQm9HItMKK/BqmnL8x46nJbVZGednxN02mlzyDjG7lipUv
+4LfQoHMCQQCUMGf9rZB8ZhP3RNx+wz2HFfVl8b38PLaJQ6E4WFj6Xrmnx/11+B1V
+yRWbWAJZueBa3DMf4cOMtdIOsH18d/+nAkBDEhoTRnQjDqX8qqr9XeK9GrpzHJXi
+aJf2ZPL8rXSpnCfCXAk4os4ntFAxuRshdDW6ed3CZqa9iDqcVl1JPqUbAkAkyfen
+JMWv/G+MfY338mR9+teXXXJ7Al+WqDGIGXbNgWK54o5sERLHT0qL7Ed5Gwo1xGD0
+00mGz0S83NfqZKgVAkA1FRhnKSKeTIApLSF0czX+CkO5b94wvB+UEj0BBLgCZOjr
+FtM5YV3W2Q/dvCxcRie65sHIil4B7E4D7pzKf1aK
+-----END RSA PRIVATE KEY-----
+)";
+
+const char *public_key_pem = R"(
+-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgFs44W15Opn0ruNjtl8fY6eNmQir
+3AsKqj2UmZ7ShLegwob3oN1lsn0keL401eOzjv2t7ObD/stszZao4Ugmccb83ITj
+EmfvHgH5rixn4+WT8sa3KW265IrnlfHjVRpGxRqGISf9ClyiK01acc+2EbUHqXyP
++JYQwWf4+ooCYDgFAgMBAAE=
+-----END PUBLIC KEY-----
+)";
+
+String encrypt_message(const String &message, const char *public_key_pem);
+String decrypt_message(const String &encrypted_message, const char *private_key_pem);
+void send_message(const String &msg_to_send);
+void on_receive_message(const String &msg_received);
+bool endsWith(const std::string &str, const std::string &suffix);
+std::string remove_last_n_chars(const std::string &str, int n);
+bool is_base64(const String &str);
+bool is_base64(const unsigned char *str, size_t length);
+String base64_encode(const unsigned char *data, size_t length);
+String base64_decode(const String &encoded_string);
+// ------------------------------------------------------------------
 BluetoothSerial SerialBT;
-bool endsWith(const std::string &str, const std::string &suffix) {
-  if (str.length() < suffix.length()){
+
+void setup()
+{
+  Serial.begin(9600);
+  SerialBT.begin("ESP32"); // Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+std::string msgToSendBuffer = "";
+std::string msgReceivedBuffer = "";
+
+void loop()
+{
+  while (Serial.available())
+  {
+    char c = Serial.read();
+    msgToSendBuffer += c;
+    if (endsWith(msgToSendBuffer, SEPERATOR.c_str()))
+    {
+      String msg = String(remove_last_n_chars(msgToSendBuffer, SEPERATOR.length()).c_str());
+      send_message(msg);
+      msgToSendBuffer = "";
+    }
+  }
+  while (SerialBT.available())
+  {
+    char c = SerialBT.read();
+    msgReceivedBuffer += c;
+    if (endsWith(msgReceivedBuffer, SEPERATOR.c_str()))
+    {
+      String received_msg = String(remove_last_n_chars(msgReceivedBuffer, SEPERATOR.length()).c_str());
+      on_receive_message(received_msg);
+      msgReceivedBuffer = "";
+    }
+  }
+  delay(1);
+}
+
+// ------------------------------------------------------------------
+void send_message(const String &msg_to_send)
+{
+  Serial.print(">>> Sending Encrypted Message:\n--------------{ Clear Message }\n"+msg_to_send);
+  String encrypted_message = encrypt_message(msg_to_send, public_key_pem);
+  if (!encrypted_message.isEmpty())
+  {
+    // Send `encrypted_message` through your communication channel
+    Serial.print("\n--------------{ Encrypted Base64 Message }\n" + encrypted_message + "\n--------------|END|\n\n");
+    SerialBT.print(encrypted_message + SEPERATOR);
+
+  }
+  else
+  {
+    Serial.println("Failed to encrypt message.");
+  }
+}
+void on_receive_message(const String &msg_received)
+{
+  Serial.print("<<< Receiving Encrypted Message:\n--------------{ Encrypted Base64 Message }\n"+msg_received);
+  String decrypted_message = decrypt_message(msg_received, private_key_pem);
+  if (!decrypted_message.isEmpty())
+  {
+    Serial.println("\n--------------{ Clear Message }\n" + decrypted_message + "\n--------------|END|\n\n");
+  }
+  else
+  {
+    Serial.println("Failed to decrypt message.");
+  }
+}
+
+String encrypt_message(const String &message, const char *public_key_pem)
+{
+  mbedtls_pk_context pk;
+  mbedtls_pk_init(&pk);
+  mbedtls_ctr_drbg_context ctr_drbg;
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  mbedtls_entropy_context entropy;
+  mbedtls_entropy_init(&entropy);
+
+  const char *pers = "rsa_encrypt";
+  mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+                        reinterpret_cast<const unsigned char *>(pers), strlen(pers));
+
+  if (mbedtls_pk_parse_public_key(&pk, (const unsigned char *)public_key_pem, strlen(public_key_pem) + 1) != 0)
+  {
+    Serial.println("Failed to parse public key.");
+    return "";
+  }
+
+  size_t encrypted_size = mbedtls_pk_get_len(&pk);
+  unsigned char encrypted[encrypted_size];
+  size_t olen = 0;
+
+  if (mbedtls_pk_encrypt(&pk, (const unsigned char *)message.c_str(), message.length(),
+                         encrypted, &olen, sizeof(encrypted),
+                         mbedtls_ctr_drbg_random, &ctr_drbg) != 0)
+  {
+    Serial.println("Encryption failed.");
+    return "";
+  }
+
+  mbedtls_pk_free(&pk);
+  mbedtls_ctr_drbg_free(&ctr_drbg);
+  mbedtls_entropy_free(&entropy);
+
+  return base64_encode(encrypted, olen);
+}
+
+String decrypt_message(const String &encrypted_base64_message, const char *private_key_pem)
+{
+  String encrypted_message = base64_decode(encrypted_base64_message);
+  mbedtls_pk_context pk;
+  mbedtls_pk_init(&pk);
+
+  if (mbedtls_pk_parse_key(&pk, (const unsigned char *)private_key_pem, strlen(private_key_pem) + 1, nullptr, 0) != 0)
+  {
+    Serial.println("Failed to parse private key.");
+    return "";
+  }
+
+  size_t encrypted_size = encrypted_message.length();
+  unsigned char decrypted[encrypted_size];
+  size_t olen = 0;
+
+  if (mbedtls_pk_decrypt(&pk, (const unsigned char *)encrypted_message.c_str(), encrypted_size,
+                         decrypted, &olen, sizeof(decrypted),
+                         nullptr, nullptr) != 0)
+  {
+    Serial.println("Decryption failed.");
+    return "";
+  }
+
+  mbedtls_pk_free(&pk);
+
+  return String((char *)decrypted, olen);
+}
+
+bool endsWith(const std::string &str, const std::string &suffix)
+{
+  if (str.length() < suffix.length())
+  {
     return false;
   }
   return str.compare(str.length() - suffix.length(), suffix.length(), suffix) == 0;
 }
-
-void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+std::string remove_last_n_chars(const std::string &str, int n)
+{
+  if (n <= 0)
+  {
+    return str; // If n is zero or negative, return the original string
+  }
+  if (n >= str.length())
+  {
+    return ""; // If n is greater than or equal to the string length, return an empty string
+  }
+  return str.substr(0, str.length() - n); // Return the string without the last n characters
 }
 
-void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
+bool is_base64(const String &str)
+{
+  // Check if the string length is a multiple of 4
+  if (str.length() % 4 != 0)
+  {
+    Serial.println("String length is not a multiple of 4.");
+    return false;
   }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+
+  // Check if the string contains only valid base64 characters
+  for (size_t i = 0; i < str.length(); ++i)
+  {
+    char c = str.charAt(i);
+    if (!((c >= 'A' && c <= 'Z') ||
+          (c >= 'a' && c <= 'z') ||
+          (c >= '0' && c <= '9') ||
+          (c == '+') ||
+          (c == '/') ||
+          (c == '=')))
+    {
+      Serial.print("Invalid base64 character found: ");
+      Serial.println("found:<"+String(c)+"> at index: <"+String(i)+">");
+      return false;
+    }
   }
-  delay(20);
+
+  return true;
 }
+bool is_base64(const unsigned char *str, size_t length)
+{
+  if (length % 4 != 0)
+  {
+    Serial.println("String length is not a multiple of 4.");
+    return false;
+  }
+
+  for (size_t i = 0; i < length; ++i)
+  {
+    unsigned char c = str[i];
+    if (!((c >= 'A' && c <= 'Z') ||
+          (c >= 'a' && c <= 'z') ||
+          (c >= '0' && c <= '9') ||
+          (c == '+') ||
+          (c == '/') ||
+          (c == '=')))
+    {
+      Serial.print("Invalid base64 character found: ");
+      Serial.println("found:<"+String(c)+"> at index: <"+String(i)+">");
+
+      return false;
+    }
+  }
+
+  return true;
+}
+
+String base64_encode(const unsigned char *data, size_t length)
+{
+  // Calculate the length of the encoded string
+  size_t encoded_length = 4 * ((length + 2) / 3);
+  char *encoded_data = new char[encoded_length + 1]; // +1 for null terminator
+
+  // Encode the data
+  size_t output_length = 0;
+  if (mbedtls_base64_encode(reinterpret_cast<unsigned char *>(encoded_data), encoded_length + 1, &output_length, data, length) != 0)
+  {
+    Serial.println("Base64 encoding failed.");
+    delete[] encoded_data;
+    return "";
+  }
+
+  String encoded_string = String(encoded_data);
+  delete[] encoded_data;
+  return encoded_string;
+}
+String base64_decode(const String &encoded_string)
+{
+  size_t encoded_length = encoded_string.length();
+  size_t decoded_length = (encoded_length / 4) * 3;
+  unsigned char *decoded_data = new unsigned char[decoded_length];
+
+  size_t output_length = 0;
+  if (mbedtls_base64_decode(decoded_data, decoded_length, &output_length,
+                            reinterpret_cast<const unsigned char *>(encoded_string.c_str()), encoded_length) != 0)
+  {
+    Serial.println("Base64 decoding failed.");
+    is_base64(encoded_string);
+    delete[] decoded_data;
+    return "";
+  }
+
+  String decoded_string = String(reinterpret_cast<char *>(decoded_data), output_length);
+  delete[] decoded_data;
+  return decoded_string;
+}
+// ------------------------------------------------------------------
